a70c2fee7812a84c46c35f3b69c2abded11bbe85
cdcvidal <celine_vidal@natural-solutions.eu>
2019-05-16 11:39:04 +0200

    Add id list habitat and site_type

--- config/conf_schema_toml.py
+++ config/conf_schema_toml.py
@@ -13,6 +13,8 @@ pagination_serverside= False
 items_per_page = 10
 plot_size_unite = "mètre"
 MODULE_CODE = "SUIVI_HAB_STA",
+id_bib_list_habitat = 2,
+site_type = 609,
 
 site_message = {"emptyMessage" : "Aucun site à afficher ", "totalMessage" : "sites(s) au total"}
 list_visit_message = {"emptyMessage" : "Aucune visite sur ce site ", "totalMessage" : "visites au total"}
@@ -45,3 +47,5 @@ class GnModuleSchemaConf(Schema):
     pagination_serverside = fields.Boolean(missing=pagination_serverside)
     items_per_page = fields.Integer(missing=items_per_page)
     plot_size_unite = fields.String(missing=plot_size_unite)
+    id_bib_list_habitat = fields.Integer(missing=id_bib_list_habitat)
+    site_type = fields.Integer(missing=site_type)
